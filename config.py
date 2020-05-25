e2624ddb1e9426e5c686d8312e9fcc7c32cb8464
Edgard Decena <edecena@gmail.com>
2020-05-25 10:07:53 -0400

    Fix config.py

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 """
 Little MVC Framework.
+Created by Edgard Decena.
+Email: edecena@gmail.com
 
 Configurations.
 """
-
+# +---------------------------------------------------------------------------+
+# |                         YOUR CONSTANTS GO DOWN HERE                       |
+# +---------------------------------------------------------------------------+
 
 if __name__ == "__main__":
     # Testing ...
