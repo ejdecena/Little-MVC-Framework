bb10d0dc56b3e4e2cd2277455c672d426fd16772
Edgard Decena <edecena@gmail.com>
2020-05-25 20:10:08 -0400

    Adding execute_db_query()

--- helpers.py
+++ helpers.py
@@ -6,6 +6,9 @@ Email: edecena@gmail.com
 
 Helpers implementation.
 """
+import sqlite3
+import typing
+
 
 def load_object(module: str) -> callable:
     """
@@ -30,8 +33,19 @@ def load_object(module: str) -> callable:
 
 
 class Singleton:
-    """Singleton class decorator."""
+    """Singleton class decorator.
+    
+    Use:
+        @Singleton
+        class Test:
+            def __init__(self, text):
+                self.text = text
 
+        a = Test("Hello")
+        b = Test("World")
+
+        print("id(a):", id(a), "id(b):", id(b), "Diff:", id(a)-id(b))
+    """
     def __init__(self, cls):
         self.__cls = cls
         self.__obj = None
@@ -40,6 +54,30 @@ class Singleton:
         if not self.__obj:
             self.__obj = self.__cls(*args, **kwargs)
         return self.__obj
+
+
+def execute_db_query(db_filename: str, query: str, parameters: tuple=()) \
+                    -> typing.Any:
+    """Execute a query in a SQLite-type database.
+    
+    Args:
+        db_filename: string with the name of the SQLite file.
+        query: strig with the SQL query.
+        parameters: tupla with the query parameters.
+
+    Returns:
+        a tuple of data in case of SELECT queries or an integer with the number of affected rows.
+
+    Use:
+        query      = "INSERT INTO contacts VALUES (NULL, ?, ?);"
+        parameters = (namefield, numfield)
+        result     = execute_db_query(query, parameters)
+    """
+    with sqlite3.connect(db_filename) as conn:
+        cursor       = conn.cursor()
+        query_result = cursor.execute(query, parameters)
+        conn.commit()
+    return query_result
 
 # +---------------------------------------------------------------------------+
 # |                         YOUR HELPERS GO DOWN HERE                         |
